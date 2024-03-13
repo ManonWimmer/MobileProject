158e8d407f0ff6af8e747001c09ed6957f85ba4a
LordBrach <alessandrotano@outlook.fr>
2024-03-14 00:29:29 +0100

    Star VFX fix & Google Test Scene

---
+++ Assets/Scripts/Alessandro/Google/GoogleSignInCheck.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.Events;
+using TMPro;
+
+public class GoogleSignInCheck : MonoBehaviour{
+    private bool isPlayerConnected = false;
+    public static GoogleSignInCheck instance;
+    [SerializeField] bool shouldGoogleBeActive = true;
+
+    [SerializeField] UnityEvent OnPlayerConnectingSuccess;
+    [SerializeField] UnityEvent OnPlayerConnectingFail;
+    [SerializeField] TextMeshProUGUI errorText;
+    int loginHardCap = 5;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void Start()
+    {
+        // juste un check pour debug au cas ou si ça marche pas très bien
+        if (shouldGoogleBeActive)
+        {
+            PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        }
+    }
+
+/*    internal void ProcessAuthenticationa(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            isPlayerConnected = true;
+            OnPlayerConnectingSuccess.Invoke();
+            // Continue with Play Games Services
+        }
+        else
+        {
+            OnPlayerConnectingFail.Invoke();
+            // juste histoire de pas rentrer en boucle inf de login
+            Debug.Log("Player is not connected");
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            if(loginHardCap > 0)
+                PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
+        }
+    }*/
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        loginHardCap -= 1;
+        switch (status)
+        {
+            case SignInStatus.Success:
+                isPlayerConnected = true;
+                OnPlayerConnectingSuccess.Invoke();
+                errorText.text = ("good :D");
+                break;
+            case SignInStatus.Canceled:
+                OnPlayerConnectingFail.Invoke();
+                Debug.Log("Authentication canceled by user.");
+                errorText.text = ("Authentication canceled by user.");
+                break;
+            case SignInStatus.InternalError:
+                OnPlayerConnectingFail.Invoke();
+                Debug.LogError("Internal error during authentication.");
+                errorText.text = ("Internal error during authentication.");
+
+                break;
+            default:
+                OnPlayerConnectingFail.Invoke();
+                Debug.LogWarning("Unknown error during authentication.");
+                errorText.text = ("Unknown error during authentication.");
+
+                break;
+        }
+        if (loginHardCap > 0 && status != SignInStatus.Success)
+            PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
+    }
+
+    public bool getPlayerStatus()
+    {
+        return isPlayerConnected;
+    }
+}
--- Assets/Scripts/Pierre/particleUI.cs
+++ Assets/Scripts/Pierre/particleUI.cs
@@ -7,13 +7,24 @@ public class particleUI : MaskableGraphic
 {
     [SerializeField] private ParticleSystemRenderer _particle;
     [SerializeField] private Camera _bakeCamera;
-
+    [SerializeField] bool isVerticalBillBoard = false;
+    [SerializeField] float minSize = 2.1f;
+    [SerializeField] float maxSize = 2.1f;
     // Update is called once per frame
     void Update()
     {
         SetVerticesDirty();
     }
 
+    protected override void Start()
+    {
+        if(isVerticalBillBoard)
+        {
+            _particle.renderMode = ParticleSystemRenderMode.VerticalBillboard;
+            _particle.maxParticleSize = maxSize;
+            _particle.minParticleSize = minSize;
+        }
+    }
     protected override void OnPopulateMesh(Mesh mesh)
     {
         mesh.Clear();
