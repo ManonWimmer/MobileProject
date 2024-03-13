fe8cd17b8405278def5537f12efc8585f2dd4fbd
Manon Wimmer <manonwimmer30@gmail.com>
2024-03-13 16:18:17 +0100

    4 enemy actions instead of 3

--- Assets/Scripts/Manon/Prototype/EnemyAction.cs
+++ Assets/Scripts/Manon/Prototype/EnemyAction.cs
@@ -8,7 +8,6 @@ public class EnemyAction : MonoBehaviour
 {
     // ----- FIELDS ----- //
     [SerializeField] Image _abilityIcon;
-    [SerializeField] Button _button;
     private scriptablePower _ability;
     // ----- FIELDS ----- //
 
@@ -21,21 +20,11 @@ public class EnemyAction : MonoBehaviour
         _abilityIcon.enabled = true;
 
         _abilityIcon.sprite = _ability.Icon;
-
-        if (ability.CanBeDefused)
-        {
-            _button.gameObject.SetActive(true);
-        }
-        else
-        {
-            _button.gameObject.SetActive(false);
-        }
     }
 
     public void HideEnemyAction()
     {
         Debug.Log("hide enemy action");
-        _button.gameObject.SetActive(false);
         GetComponent<Image>().enabled = false;
         _abilityIcon.enabled = false;
     }
